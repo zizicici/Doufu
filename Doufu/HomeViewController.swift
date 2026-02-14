bdb519a2854d2d7ada51c4cf666dea2b9f0c19fc
zizicici <1940699+zizicici@users.noreply.github.com>
2026-02-14 22:08:11 +0800

    [misc] Initial Project

---
+++ Doufu/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Doufu
+//
+//  Created by Salley Garden on 2026/02/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
